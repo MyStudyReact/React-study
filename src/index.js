24aacb81eb02a5b6add4e71664773ed5826e059f
lyxlwz <54365306+lyxlwz@users.noreply.github.com>
2022-09-05 22:40:27 +0800

    目录熟悉和简化

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
+// React:框架的和核心包
+// ReactDom:专门做渲染相关的包
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//应用的全局样式文件
+import './index.css';
+
+// 引入的根组件
+import App from './App';
+
+// 渲染根组件App 到  id为'Root'的dom节点上
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
