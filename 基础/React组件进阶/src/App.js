6dfb040850545d85731c2a81a1dbf01c3f40fac1
lyxlwz <1850115720@qq.com>
2022-10-20 20:15:58 +0800

    props校验-规则说明

--- 基础/React组件进阶/src/App.js
+++ 基础/React组件进阶/src/App.js
@@ -3,24 +3,28 @@ import React from 'react'
 // 里面有各种各样的内置的校验规则
 import PropTypes from 'prop-types'
 
-function Com ({ list }) {
+function Com ({ list, message, }) {
   return (
     <>
       {list.map(item => <h1 key={item}>Com,{item}</h1>)}
+
+      <h2>{message}</h2>
     </>
   )
 }
 
 Com.propTypes = {
   // 定义各种各样的规则
-  list: PropTypes.array //限定这里的list参数类型必须是数组类型
+  list: PropTypes.array, //限定这里的list参数类型必须是数组类型
+
+  message: PropTypes.string.isRequired // 加上isRequired,表示必填项
 }
 
 class App extends React.Component {
   render () {
     return (
       <>
-        <Com list={[1, 2, 3]}></Com>
+        <Com list={[1, 2, 3]} message={'我过来了！'}></Com>
       </>
     )
   }
