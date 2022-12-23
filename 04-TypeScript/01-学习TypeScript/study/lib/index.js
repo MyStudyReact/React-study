c56fa934a316d52761124f4cca70405a383900c3
lyxlwz <1850115720@qq.com>
2022-12-23 11:02:29 +0800

    ✨ 三斜线指令

---
+++ 04-TypeScript/01-学习TypeScript/study/lib/index.js
@@ -0,0 +1,13 @@
+var A;
+(function (A) {
+    A.a = 2;
+})(A || (A = {}));
+var A;
+(function (A) {
+    A.b = 5;
+})(A || (A = {}));
+var A;
+(function (A) {
+    A.c = 666;
+})(A || (A = {}));
+console.log(A);
